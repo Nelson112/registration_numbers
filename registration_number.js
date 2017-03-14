354d240537009c1be05d1864aa09d36afe649a28
Nelson112 <nelsonmgemgwana@gmail.com>
2017-03-14 16:40:02 +0200

    starters

--- registration_number.js
+++ registration_number.js
@@ -1,11 +1,43 @@
+const textInput = document.querySelector('#regNo');
+const addBtn = document.querySelector('#btn');
+const textOutput = document.querySelector('#outputReg');
+// const newReg = document.createElement('li');
+// const newReg = document.querySelector('.sample').cloneNode();
+// console.log(newReg);
+addBtn.addEventListener("click",function(){
+  const newReg = document.querySelector('.sample').cloneNode();
+  newReg.innerHTML = textInput.value;
+  textOutput.appendChild(newReg);
+  textInput.value = "";
+
+  return
+});
+
+
+
+/*
+
+// var regStore = {};
+
 var text = document.getElementById('regNo')
 var regNumber = document.createElement("li")
 var outputReg = document.getElementById('outputReg');
 
 function regFunc() {
-  outputReg.appendChild(regNumber)
-  regNumber.textContent = text.value
+
+  // if (regStore[regNo.value] === undefined) {
+  //
+  //   regStore[regNo.value] = true;
+  // } else if (regStore[regNo.value] !== undefined) {
+  //   regStore[regNo.value] = " ";
+  // }
+
+   outputReg.appendChild(regNumber)
+
+  regNumber.textContent = regNo.value;
+  // emtying the textbox
   document.getElementById("regNo").value = "";
 
   return;
 }
+ */
